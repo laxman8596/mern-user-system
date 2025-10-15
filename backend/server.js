31fd4121fa78daa9ee369f233cecf219250d7d4f
annaboinalaxman7-afk <annaboinalaxman7@gmail.com>
2025-10-15 18:31:02 +0530

    added frontend url in backend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: [
-        'https://mern-user-system-frontend.vercel.app',
+        'https://user-system-frontend.vercel.app',
         'http://localhost:5173',
         'http://localhost:3000'
     ],
