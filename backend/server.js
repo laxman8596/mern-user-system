3ce07338dc05fb1c94c911c81ae967321e2a811a
annaboinalaxman7-afk <annaboinalaxman7@gmail.com>
2025-10-15 18:34:01 +0530

    changes fixed

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,9 @@ app.use(cors({
         'http://localhost:5173',
         'http://localhost:3000'
     ],
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Root route (no DB required)
